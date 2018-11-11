ec5330f599df8926098f41667014fc24464f22c3
cinness <44888344+cinness@users.noreply.github.com>
2018-11-11 01:06:02 -0600

    Update multituneplayer-increase.js

--- multituneplayer-increase.js
+++ multituneplayer-increase.js
@@ -1,4 +1,18 @@
 window.onload = function littleplayer() {
+    
+    
+        function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
 
     // get user input info
 
@@ -184,17 +198,6 @@ window.onload = function littleplayer() {
 
   // end
     
-    function unfade(element) {
-    var op = 0.1;  // initial opacity
-    element.style.display = 'block';
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 10);
-}
+
 
 };
