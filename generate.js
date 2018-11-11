ac33086957b099596a25a14c09cc3e09269ebfcb
cinness <44888344+cinness@users.noreply.github.com>
2018-11-11 03:38:18 -0600

    Update generate.js

--- generate.js
+++ generate.js
@@ -37,7 +37,7 @@ function generate() {
     document.getElementById("index-color-select").style.display = "none";
     document.getElementById("codebox").style.display = "block";
     document.getElementById("desc").style.textAlign = "center";
-    document.getElementById("desc").innerHTML = "Copy and paste the code below into your blog or website's HTML!"
+    document.getElementById("desc").innerHTML = "Copy and paste the code below into your blog or website's HTML"
 
     // declare variables for xlittleplayerinfo and xlittleplayernames spans
 
