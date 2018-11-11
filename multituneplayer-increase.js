1569a9458df9ce839ba5c0b377686f554ba6a50f
cinness <44888344+cinness@users.noreply.github.com>
2018-11-11 00:57:05 -0600

    Update multituneplayer-increase.js

--- multituneplayer-increase.js
+++ multituneplayer-increase.js
@@ -84,14 +84,14 @@ window.onload = function littleplayer() {
     pbar1.style.marginRight = "2px";
     pbar1.style.width = "3px";
     pbar1.style.height = "10px";
-    pbar1.style.marginTop = "1px";
+    pbar1.style.marginTop = "0px";
     pbar1.style.background = songs[0];
 
     pbar2.style.display = "block";
     pbar2.style.float = "left";
     pbar2.style.width = "3px";
     pbar2.style.height = "10px";
-    pbar2.style.marginTop = "1px";
+    pbar2.style.marginTop = "0px";
     pbar2.style.background = songs[0];
 
     var xskip = document.getElementById("xskip");
