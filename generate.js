242287b5b9e5f51ebbba92b9ad597dc220bbd9ec
cinness <44888344+cinness@users.noreply.github.com>
2018-11-11 03:33:50 -0600

    Update generate.js

--- generate.js
+++ generate.js
@@ -74,7 +74,7 @@ function generate() {
 
         // generate user code in textarea
         <!-- project forked @ https://cinness.github.io/index.html -->
-        document.getElementById("codebox").innerHTML = "&lt;script src&equals;&quot;https://cinness.github.io/multituneplayer.js&quot;&gt;&lt;&sol;script&gt;&lt;div id&equals;&quot;xlittleplayer&quot;&gt;&lt;span id&equals;&quot;xlittleplayerinfo&quot; style&equals;&quot;display:none;&quot;&gt;" + oneinfonew + "&lt;&sol;span&gt;&lt;span id&equals;&quot;xlittleplayernames&quot; style&equals;&quot;display:none;&quot;&gt;" + twoinfonew + "&lt;&sol;span&gt;&lt;&sol;div&gt;";
+        document.getElementById("codebox").innerHTML = "<!-- project forked @ https://cinness.github.io/index.html --> &lt;script src&equals;&quot;https://cinness.github.io/multituneplayer.js&quot;&gt;&lt;&sol;script&gt;&lt;div id&equals;&quot;xlittleplayer&quot;&gt;&lt;span id&equals;&quot;xlittleplayerinfo&quot; style&equals;&quot;display:none;&quot;&gt;" + oneinfonew + "&lt;&sol;span&gt;&lt;span id&equals;&quot;xlittleplayernames&quot; style&equals;&quot;display:none;&quot;&gt;" + twoinfonew + "&lt;&sol;span&gt;&lt;&sol;div&gt;";
 
     })();
 }
