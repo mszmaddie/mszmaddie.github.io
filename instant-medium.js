0e0bb9fa1ef07cd408e29fc4b8d843b49c37fc9f
cinness <44888344+cinness@users.noreply.github.com>
2018-11-12 03:36:35 -0600

    Update and rename ind.js to instant-medium.js

--- instant-medium.js
+++ instant-medium.js
@@ -1,12 +1,16 @@
-window.onload = function littleplayer() {
+     // code forked at https://cinness.github.io/index.html
 
+mtp();
+
+function mtp() {
+        
     // get user input info
 
-    var initinfo = document.getElementById("xlittleplayerinfo").innerHTML;
+    var initinfo = document.getElementById("multi-info").innerHTML;
     var songs = initinfo.split(" ");
     var sq = songs.length - 1;
 
-    var initnames = document.getElementById("xlittleplayernames").innerHTML;
+    var initnames = document.getElementById("multi-names").innerHTML;
     var tracks = initnames.split(",");
 
    // fill holes left by possible blank track titles
@@ -24,13 +28,13 @@ window.onload = function littleplayer() {
 
     // set up DOM
 
-        document.getElementById("xlittleplayer").innerHTML = '<audio id="xaudio" preload="auto"><source src="' + songs[2] +'" type="audio/mpeg"></audio><div id="controls1"><div id="xplay"></div><div id="xpause"><div id="pbar1"></div><div id="pbar2"></div><div id="kr2"></div></div></div><div id="controls2"><div id="xskip"><div id="sk1"></div><div id="sk2"></div><div id="kr3"></div></div></div><div id="xtrackname"></div><div id="kr"></div>';
+        document.getElementById("mtplayer").innerHTML = '<audio id="xaudio" preload="auto"><source src="' + songs[2] +'" type="audio/mpeg"></audio><div id="controls1"><div id="xplay"></div><div id="xpause"><div id="pbar1"></div><div id="pbar2"></div><div id="kr2"></div></div></div><div id="controls2"><div id="xskip"><div id="sk1"></div><div id="sk2"></div><div id="kr3"></div></div></div><div id="xtrackname"></div><div id="kr"></div>';
 
     // style player elements
 
-    document.getElementById("xlittleplayer").style.display = "block";
-    document.getElementById("xlittleplayer").style.width = "20px";
-    document.getElementById("xlittleplayer").style.padding = "3px 5px";
+    document.getElementById("mtplayer").style.display = "block";
+    document.getElementById("mtplayer").style.width = "25px";
+    document.getElementById("mtplayer").style.padding = "0px 15px";
 
     var xaudio = document.getElementById("xaudio");
     xaudio.style.display = "none";
@@ -51,11 +55,11 @@ window.onload = function littleplayer() {
     xtrackname.style.display = "block";
     xtrackname.style.textAlign = "left";
     xtrackname.style.fontFamily = "arial, sans-serif";
-    xtrackname.style.fontSize = "11px";
+    xtrackname.style.fontSize = "12px";
     xtrackname.style.fontWeight = "600";
     xtrackname.style.lineHeight = "13px";
-    xtrackname.style.marginTop = "-2px";
-    xtrackname.style.marginLeft = "28px";
+    xtrackname.style.marginTop = "-1px";
+    xtrackname.style.marginLeft = "32px";
     xtrackname.style.width = "90px";
     xtrackname.style.position = "absolute";
     xtrackname.style.overflow = "hidden";
@@ -67,9 +71,9 @@ window.onload = function littleplayer() {
     xplay.style.display = "block";
     xplay.style.width = "0px";
     xplay.style.height = "0px";
-    xplay.style.borderTop = "4px solid transparent";
-    xplay.style.borderLeft = "6px solid " + songs[0];
-    xplay.style.borderBottom = "4px solid transparent";
+    xplay.style.borderTop = "5px solid transparent";
+    xplay.style.borderLeft = "7px solid " + songs[0];
+    xplay.style.borderBottom = "5px solid transparent";
     xplay.style.cursor = "pointer";
 
     var xpause = document.getElementById("xpause");
@@ -82,16 +86,16 @@ window.onload = function littleplayer() {
     pbar1.style.display = "block";
     pbar1.style.float = "left";
     pbar1.style.marginRight = "2px";
-    pbar1.style.width = “4px";
-    pbar1.style.height = “8px";
-    pbar1.style.marginTop = "1px";
+    pbar1.style.width = "3px";
+    pbar1.style.height = "9px";
+    pbar1.style.marginTop = "0.5px";
     pbar1.style.background = songs[0];
 
     pbar2.style.display = "block";
     pbar2.style.float = "left";
-    pbar2.style.width = “4px";
-    pbar2.style.height = “8px";
-    pbar2.style.marginTop = "1px";
+    pbar2.style.width = "3px";
+    pbar2.style.height = "9px";
+    pbar2.style.marginTop = "0.5px";
     pbar2.style.background = songs[0];
 
     var xskip = document.getElementById("xskip");
@@ -105,17 +109,17 @@ window.onload = function littleplayer() {
     sk1.style.float = "left";
     sk1.style.width = "0px";
     sk1.style.height = "0px";
-    sk1.style.borderTop = "4px solid transparent";
-    sk1.style.borderLeft = "4px solid " + songs[0];
-    sk1.style.borderBottom = "4px solid transparent";
+    sk1.style.borderTop = "5px solid transparent";
+    sk1.style.borderLeft = "5px solid " + songs[0];
+    sk1.style.borderBottom = "5px solid transparent";
 
     sk2.style.display = "block";
     sk2.style.float = "left";
     sk2.style.width = "0px";
     sk2.style.height = "0px";
-    sk2.style.borderTop = "4px solid transparent";
-    sk2.style.borderLeft = "4px solid " + songs[0];
-    sk2.style.borderBottom = "4px solid transparent";
+    sk2.style.borderTop = "5px solid transparent";
+    sk2.style.borderLeft = "5px solid " + songs[0];
+    sk2.style.borderBottom = "5px solid transparent";
 
     // check autoplay preference
 
@@ -182,6 +186,8 @@ window.onload = function littleplayer() {
         skipend();
     };
 
-    // end
+  // end
+    
+
 
 };
