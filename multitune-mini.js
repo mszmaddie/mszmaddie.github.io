3ef22fe6f3f823d9d043c9fcf57939b4153e6973
cinness <44888344+cinness@users.noreply.github.com>
2018-11-12 03:26:57 -0600

    Update multitune-mini.js

--- multitune-mini.js
+++ multitune-mini.js
@@ -86,14 +86,14 @@ window.onload = function mtp() {
     pbar1.style.marginRight = "2px";
     pbar1.style.width = "2px";
     pbar1.style.height = "7px";
-    pbar1.style.marginTop = "1px";
+    pbar1.style.marginTop = "0px";
     pbar1.style.background = songs[0];
 
     pbar2.style.display = "block";
     pbar2.style.float = "left";
     pbar2.style.width = "2px";
     pbar2.style.height = "7px";
-    pbar2.style.marginTop = "1px";
+    pbar2.style.marginTop = "0px";
     pbar2.style.background = songs[0];
 
     var xskip = document.getElementById("xskip");
