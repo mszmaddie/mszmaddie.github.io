2bb5587307bd38ad02254634fd274507ea475b55
cinness <44888344+cinness@users.noreply.github.com>
2018-11-13 12:12:36 -0600

    Update generate-medium.js

--- generate-medium.js
+++ generate-medium.js
@@ -78,7 +78,7 @@ function generate() {
         document.getElementById("codebox").innerHTML = "<!-- code forked at https://cinness.github.io/medium.html --> &lt;script src&equals;&quot;https://cinness.github.io/instant-medium.js&quot;&gt;&lt;&sol;script&gt;&lt;div id&equals;&quot;mtplayer&quot;&gt;&lt;span id&equals;&quot;multi-info&quot; style&equals;&quot;display:none;&quot;&gt;" + oneinfonew + "&lt;&sol;span&gt;&lt;span id&equals;&quot;multi-names&quot; style&equals;&quot;display:none;&quot;&gt;" + twoinfonew + "&lt;&sol;span&gt;&lt;&sol;div&gt;";
 
         document.getElementById("codebox1").innerHTML = "<!-- code forked at https://cinness.github.io/medium.html --> &lt;script src&equals;&quot;https://cinness.github.io/instant-medium.js&quot;&gt;&lt;&sol;script&gt;&lt;div id&equals;&quot;mtplayer&quot;&gt;&lt;span id&equals;&quot;multi-info&quot; style&equals;&quot;display:none;&quot;&gt;" + oneinfonew + "&lt;&sol;span&gt;&lt;span id&equals;&quot;multi-names&quot; style&equals;&quot;display:none;&quot;&gt;" + twoinfonew + "&lt;&sol;span&gt;&lt;&sol;div&gt;";
-        
+        })
         
     })();
 
